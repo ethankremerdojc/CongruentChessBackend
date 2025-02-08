507553a73f6401867e4b9dc223ee17e938b8bf3a
Ethan Kremer <64492390+ethankremerdojc@users.noreply.github.com>
2025-02-07 17:42:35 -0700

    working validation

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ app = FastAPI()
 # Store active WebSocket connections
 active_connections: List[WebSocket] = []
 
+from validation import *
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
@@ -14,7 +16,38 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_text()
-            for connection in active_connections:
-                await connection.send_text(data)  # Broadcast the message
-    except:
+
+            # check if is fen
+            print(data)
+            # check if there are 7 slashes in the data, is fen.
+            try:
+                if data.count("/") == 7:
+                    fen, from_pos_str, to_pos_str = data.split("|")
+
+
+
+                    board = decode_fen_to_board(fen)
+                    from_pos = (int(from_pos_str[0]), int(from_pos_str[2]))
+                    to_pos = (int(to_pos_str[0]), int(to_pos_str[2]))
+
+                    if not is_valid_move(board, from_pos, to_pos):
+                        print("Invalid move!")
+                        continue
+
+                    board[to_pos[1]][to_pos[0]] = board[from_pos[1]][from_pos[0]]
+                    board[from_pos[1]][from_pos[0]] = None
+
+                    fen = encode_board_to_fen(board)
+                    print(fen)
+                    print("Succeed!")
+                    for connection in active_connections:
+                        await connection.send_text(fen)
+                        # Figure out if I want some sort of success message instead
+
+            except Exception as e:
+                print(e)
+                continue
+            
+    except Exception as e:
+        print(e)
         active_connections.remove(websocket)
---
+++ validation.py
@@ -0,0 +1,158 @@
+PIECE_FOR_LETTER = {
+    "p": "pawn",
+    "r": "rook",
+    "n": "knight",
+    "b": "bishop",
+    "q": "queen",
+    "k": "king"
+}
+
+def decode_fen_to_board(fen):
+    board = []
+    rows = fen.split("/")
+    for row in rows:
+        board_row = []
+        for char in row:
+            if char.isdigit():
+                board_row.extend([None] * int(char))
+            else:
+                board_row.append(char)
+        board.append(board_row)
+    return board
+
+def encode_board_to_fen(board):
+    fen = []
+    for row in board:
+        empty = 0
+        fen_row = ""
+        for cell in row:
+            if cell is None:
+                empty += 1
+            else:
+                if empty > 0:
+                    fen_row += str(empty)
+                    empty = 0
+                fen_row += cell
+        if empty > 0:
+            fen_row += str(empty)
+        fen.append(fen_row)
+    return "/".join(fen)
+
+def get_legal_moves(piece_type, color, position):
+    if piece_type == "pawn":
+        return get_legal_pawn_moves(color, position)
+    elif piece_type == "rook":
+        return get_legal_rook_moves(position)
+    elif piece_type == "knight":
+        return get_legal_knight_moves(position)
+    elif piece_type == "bishop":
+        return get_legal_bishop_moves(position)
+    elif piece_type == "queen":
+        return get_legal_queen_moves(position)
+    elif piece_type == "king":
+        return get_legal_king_moves(position)
+    else:
+        raise ValueError("Invalid piece type")
+
+def get_legal_pawn_moves(color, position):
+    x, y = position
+    direction = -1 if color == "white" else 1
+
+    legal_moves = []
+    legal_moves.append((x, y + direction))
+
+    if (y == 1 and color == "black") or (y == 6 and color == "white"):
+        legal_moves.append((x, y + 2 * direction))
+
+    if x > 0:
+        legal_moves.append((x - 1, y + direction))
+    if x < 7:
+        legal_moves.append((x + 1, y + direction))
+
+    return legal_moves
+
+def get_legal_rook_moves(position):
+    x, y = position
+    legal_moves = []
+
+    for i in range(8):
+        if i != x:
+            legal_moves.append((i, y))
+        if i != y:
+            legal_moves.append((x, i))
+
+    return legal_moves
+
+def get_legal_knight_moves(position):
+    x, y = position
+    legal_moves = []
+
+    possible_moves = [
+        (x + 1, y + 2), (x + 2, y + 1), (x + 2, y - 1), (x + 1, y - 2),
+        (x - 1, y - 2), (x - 2, y - 1), (x - 2, y + 1), (x - 1, y + 2)
+    ]
+
+    for move in possible_moves:
+        mx, my = move
+        if 0 <= mx < 8 and 0 <= my < 8:
+            legal_moves.append(move)
+
+    return legal_moves
+
+def get_legal_bishop_moves(position):
+    x, y = position
+    legal_moves = []
+
+    for i in range(1, 8):
+        if x + i < 8 and y + i < 8:
+            legal_moves.append((x + i, y + i))
+        if x + i < 8 and y - i >= 0:
+            legal_moves.append((x + i, y - i))
+        if x - i >= 0 and y + i < 8:
+            legal_moves.append((x - i, y + i))
+        if x - i >= 0 and y - i >= 0:
+            legal_moves.append((x - i, y - i))
+
+    return legal_moves
+
+def get_legal_queen_moves(position):
+    return get_legal_rook_moves(position) + get_legal_bishop_moves(position)
+
+def get_legal_king_moves(position):
+    x, y = position
+    legal_moves = []
+
+    possible_moves = [
+        (x + 1, y), (x + 1, y + 1), (x, y + 1), (x - 1, y + 1),
+        (x - 1, y), (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)
+    ]
+
+    for move in possible_moves:
+        mx, my = move
+        if 0 <= mx < 8 and 0 <= my < 8:
+            legal_moves.append(move)
+
+    return legal_moves
+
+def is_valid_move(board, from_pos, to_pos):
+    try:
+        piece = board[from_pos[1]][from_pos[0]]
+
+        if piece is None:
+            return False
+
+        if piece.isupper():
+            color = "white"
+        else:
+            color = "black"
+                        
+        piece_type = PIECE_FOR_LETTER[piece.lower()]
+        legal_moves = get_legal_moves(piece_type, color, from_pos)
+
+        if to_pos not in legal_moves:
+            return False
+
+        return True
+
+    except:
+        return False
